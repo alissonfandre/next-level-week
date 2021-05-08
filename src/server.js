63c59ffd0c4c1ffe171350186dae8ed598f1be6d
Alisson Feliciano André <alissonfeliciano0@gmail.com>
2021-05-07 23:36:49 -0300

    aprendendo como utilizar node.js

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica 
+server.use(express.static("public"))
+
+
+//utilizando template engine 
+
+const nunjucks = require("nunjucks")
+
+nunjucks.configure("src/views", {
+    express: server, 
+    noCache: true
+})
+
+//configurar caminhos da minha aplicação
+//pagina inicial 
+server.get("/", (req, res) => {
+    return res.render("index.html", {title: "Um título"})
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+server.listen(3000)
